f603c42a0d4cbd88c3112f63a63487cc65ac700d
Austin Cripe <aaustinc@live.com>
2023-09-21 12:15:29 -0400

    aded react app

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ const pg = require('pg')
 const client = new pg.Client('postgres://localhost/fullstack_template_db')
 const express = require('express')
 const app = express()
+const path = require('path')
 
+app.get('/', (req,res,next)=>{
+    res.sendFile( path.join(__dirname + '/index.html') )
+})
+
+const reactApp = path.join(__dirname, 'dist/main.js')
+
+app.get('/dist/main.js', (req, res, next)=>{
+    res.sendFile( reactApp )
+})
 
 const init = async()=>{
     await client.connect()
